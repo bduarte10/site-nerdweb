1d8fa0acb5fd7cf6feb8628cab8e587b6270186f
Bruno Duarte <duartebruno2208@gmail.com>
2022-04-18 18:23:41 -0300

    ajuste de responsividade

--- script.js
+++ script.js
@@ -22,3 +22,17 @@ const swiper = new Swiper('.swiper', {
 		prevEl: '.swiper-button-prev',
 	},
 });
+
+let menu = document.getElementById('menu-bar');
+let toggle = document.querySelector('.mobile-menu');
+let close = document.querySelector('.close-btn');
+
+menu.addEventListener('click', () => {
+	toggle.classList.add('active');
+});
+close.addEventListener('click', () => {
+	toggle.classList.remove('active');
+});
+window.onscroll = () => {
+	toggle.classList.remove('active');
+};
