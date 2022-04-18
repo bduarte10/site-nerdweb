0612567ffa29a47d306802b4ed6f89d12e331b21
Bruno Duarte <duartebruno2208@gmail.com>
2022-04-18 12:01:53 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,24 @@
+const swiper = new Swiper('.swiper', {
+	// Optional parameters
+	direction: 'horizontal',
+	loop: true,
+	slidesPerView: 1,
+	centeredSlides: true,
+	spaceBetween: 10,
+	grabCursor: true,
+
+	// Responsive breakpoints
+	breakpoints: {
+		// when window width is >= 640px
+		640: {
+			slidesPerView: 3,
+			spaceBetween: 10,
+		},
+	},
+
+	// Navigation arrows
+	navigation: {
+		nextEl: '.swiper-button-next',
+		prevEl: '.swiper-button-prev',
+	},
+});
